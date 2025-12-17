0bb4c1f1d08ca567136e9c31867e6a489b0ccb2a
Tarun-ji <tarun2nokwal@gmail.com>
2025-12-17 19:46:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ window.addEventListener('load', () => {
                 opacity: 0,
                 duration: 1,
                 onComplete: () => {
-                    window.location.href = 'cause.html'; // Replace with the actual URL of the next page
+                    window.location.href = 'MINE.html'; // Replace with the actual URL of the next page
                 }
             });
         });
